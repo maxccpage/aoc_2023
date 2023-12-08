a63e0f01cec47763a10d22e2c17f1b8742d6f39d
Max Page <maxccpage@Maxs-MacBook-Pro.local>
2023-12-07 23:24:08 -0400

    day 4 part 1

--- src/day_4/part_1.test.ts
+++ src/day_4/part_1.test.ts
@@ -1,15 +1,12 @@
+import { readFileContents } from "../shared/utils";
+import solution from "./part_1";
 
-  import { readFileContents } from "../shared/utils";
-  import solution from "./part_1";
+describe("Day 4 Part 1", () => {
+  it("should return the correct value", () => {
+    const input = readFileContents(`./src/day_4/test_input_1.txt`);
 
-  describe ("Day 4 Part 1", () => {
-      it('should return the correct value', () => {
-          const input = readFileContents(`./src/day_4/test_input_1.txt`);
+    const expected_result = 13;
 
-          // TODO: Update this with the correct value
-          const expected_result = 0;
-
-          expect(solution(input)).toEqual(0);
-      });
-  })
-  
+    expect(solution(input)).toEqual(expected_result);
+  });
+});
--- src/day_4/part_1.ts
+++ src/day_4/part_1.ts
@@ -1,13 +1,56 @@
+import { inputIterator } from "../shared/utils";
 
-  import { inputIterator } from "../shared/utils";
+export default (input: string) => {
+  const separateNumbers = (card: string) => {
+    const stripped = card.slice(8); // rmvs card # and colon
+    const [raw_winning, raw_nums] = stripped.split("|");
 
-  export default (input: string) => {
-    let result = 0;
+    const format = (str: string): number[] =>
+      str
+        .split(/\s+/)
+        .filter((item) => item !== "")
+        .map((num) => parseInt(num));
 
-    inputIterator(input)((line) => {
-        // Code Here 
+    const all_numbers = format(raw_nums);
+
+    const winning_numbers = format(raw_winning);
+
+    return [all_numbers, winning_numbers];
+  };
+
+  const getWinningNumbersMap = (
+    winning_numbers: number[]
+  ): Map<number, number> => {
+    const winning_numbers_map = new Map();
+    winning_numbers.forEach((value, index) => {
+      winning_numbers_map.set(value, index);
+    });
+    return winning_numbers_map;
+  };
+
+  const counts: number[] = [];
+
+  inputIterator(input)((line) => {
+    const [all_numbers, winning_numbers] = separateNumbers(line);
+
+    const winning_numbers_map = getWinningNumbersMap(winning_numbers);
+
+    let count = 0;
+
+    all_numbers.forEach((num) => {
+      if (winning_numbers_map.has(num)) {
+        if (count === 0) {
+          count = 1;
+        } else {
+          count = count * 2;
+        }
+      }
     });
 
-    console.log('result --->', result);
-  }
-  
+    counts.push(count);
+  });
+
+  const result = counts.reduce<number>((acc, curr) => (acc += curr), 0);
+
+  return result;
+};
