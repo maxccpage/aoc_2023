83863dfd9a79360ee58ae9d95c57595801e22c7e
Max Page <maxccpage@Maxs-MacBook-Pro.local>
2023-12-07 22:38:59 -0400

    day 3 part 2

--- src/day_3/part_2.test.ts
+++ src/day_3/part_2.test.ts
@@ -1,15 +1,11 @@
+import { readFileContents } from "../shared/utils";
+import solution from "./part_2";
 
-  import { readFileContents } from "../shared/utils";
-  import solution from "./part_2";
+describe("Day 3 Part 2", () => {
+  it("should return the correct value", () => {
+    const input = readFileContents(`./src/day_3/test_input_2.txt`);
+    const expected_result = 467835;
 
-  describe ("Day 3 Part 2", () => {
-      it('should return the correct value', () => {
-          const input = readFileContents(`./src/day_3/test_input_2.txt`);
-
-          // TODO: Update this with the correct value
-          const expected_result = 0;
-
-          expect(solution(input)).toEqual(0);
-      });
-  })
-  
+    expect(solution(input)).toEqual(expected_result);
+  });
+});
